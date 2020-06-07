267406dc1722d834b7f1caa8b97725f9f53a8987
OtherwiseJLo <otherwisejlo@gmail.com>
2020-06-07 07:22:10 -0400

    Created constructor for game

--- src/sprouts.py
+++ src/sprouts.py
@@ -1,7 +1,16 @@
-class Sprouts:
-    def __init__(self):
-        self.board = None
-    
+import networkx as nx
+import ipycytoscape
 
-if __name__ == '__main__':
+
+class Sprouts:
+    def __init__(self, n=2):
+        self.board = nx.Graph()
+        self.board.add_nodes_from(range(n))
+    
+    def make_move(self, src, dest):
+        pass
+
+
+if __name__ == "__main__":
     print("Testing script")
+    board = Sprouts()
